1de75800634cdfc9018c8c08a116fc41142bcce6
Z <zachidk@gmail.com>
2018-09-30 22:51:23 -0700

    Made Class, added functionality

--- symboltable.cpp
+++ symboltable.cpp
@@ -21,102 +21,118 @@ struct data {
 		stream << lineNumber << endl;
 	};
 };
-vector<map<string, data> > stack;
+class SymbolTable {
 
-void insert(string tokenKey, int lN);
-data* searchTop(string key);
-data* searchAll(string key);
-void pushBST(map<string, data> bst);
-void popBST();
-void writeToFile();
-
-
-int main()
-{
-	map<string, data> bst;
-	pushBST(bst);
-
-	insert("1",1);
-	insert("2",2);
-	insert("3",3);
-	// For testing the search
-	//data *d = searchTop("1");
-	//stack[0].insert(pair<string,data>("4",*d));
-	//data *e = searchAll("1");
-	//stack[0].insert(pair<string,data>("5",*e));
-
-	writeToFile();
-
-
-}
-void insert(string tokenKey, int lN)
-{
-	if(searchTop(tokenKey) == NULL)
+	/*
+	void insert(string tokenKey, int lN);
+	data* searchTop(string key);
+	data* searchAll(string key, int *location);
+	void pushEmptyBST();
+	void pushBST(map<string, data> bst);
+	void popBST();
+	void writeToFile();
+	*/
+public: 
+	void insert(string tokenKey, int lN)
 	{
-		data d;
-		d.lineNumber = lN;
-		stack[0].insert(pair<string,data>(tokenKey,d));
-	}
-	else
-	{
-		//The identifier already exists so error?
+		int location;
+		data *d = searchAll(tokenKey,&location);
+		if(d == NULL)
+		{
+			data d;
+			d.lineNumber = lN;
+			stack[0].insert(pair<string,data>(tokenKey,d));
+		}
+		else
+		{
+			//The identifier already exists so error?
+			cout << "Shadowed variable in level: " << location << " On line number: " << d->lineNumber << endl;
+		}
+
 	}
 
-}
-
-data* searchAll(string key)
-{
-	for(vector<map<string, data> >::iterator it = stack.begin(); it != stack.end(); ++it)
+	data* searchAll(string key, int *location)
 	{
-		map<string, data>::iterator i = it->find( key );
-		if(i != it->end())
+		int count = 0;
+		for(vector<map<string, data> >::iterator it = stack.begin(); it != stack.end(); ++it)
+		{
+			map<string, data>::iterator i = it->find( key );
+			if(i != it->end())
+			{
+				// Key exists
+				//cout << "Key exists" << endl;
+				*location = count;
+				return &i->second;
+			}
+			else
+			{
+				// Key doesnt exist
+			}
+			count++;
+		}
+		return NULL;
+	}
+
+
+	data* searchTop(string key)
+	{
+		map<string, data>::iterator it = stack[0].find( key );
+		if(it != stack[0].end())
 		{
 			// Key exists
-			cout << "Key exists" << endl;
-			return &i->second;
+			//cout << "Key exists" << endl;
+			return &it->second;
 		}
 		else
 		{
 			// Key doesnt exist
+			return NULL;
 		}
 	}
-	return NULL;
-}
-
-
-data* searchTop(string key)
-{
-	map<string, data>::iterator it = stack[0].find( key );
-	if(it != stack[0].end())
+	void writeToFile()
 	{
-		// Key exists
-		cout << "Key exists" << endl;
-		return &it->second;
-	}
-	else
-	{
-		// Key doesnt exist
-		return NULL;
-	}
-}
-void writeToFile()
-{
-	ofstream stream("output.txt");
-	for(vector<map<string, data> >::iterator it = stack.begin(); it != stack.end(); ++it)
-	{
-		for(map<string, data>::iterator i = it->begin(); i != it->end(); ++i)
+		ofstream stream("output.txt");
+		for(vector<map<string, data> >::iterator it = stack.begin(); it != stack.end(); ++it)
 		{
-  			stream << i->first << " ";
-  			i->second.output(stream);
+			for(map<string, data>::iterator i = it->begin(); i != it->end(); ++i)
+			{
+					stream << i->first << " ";
+					i->second.output(stream);
+			}
 		}
+
+	}
+	void pushBST(map<string, data> bst)
+	{
+		stack.push_back(bst);
+	}
+	void pushEmptyBST()
+	{
+		map<string, data> bst;
+		stack.push_back(bst);
+	}
+	void popBST()
+	{
+		stack.pop_back();
 	}
 
-}
-void pushBST(map<string, data> bst)
+private:
+	vector<map<string, data> > stack;
+};
+// Driver for now need to test other things still
+int main()
 {
-	stack.push_back(bst);
-}
-void popBST()
-{
-	stack.pop_back();
-}
+	SymbolTable s;
+	map<string, data> bst;
+	s.pushBST(bst);
+	s.insert("1",1);
+	s.insert("2",2);
+	s.insert("3",3);
+	s.insert("1",21);
+	s.pushEmptyBST();
+	s.insert("4",21);
+
+	s.writeToFile();
+
+
+	}
