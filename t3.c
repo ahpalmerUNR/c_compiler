773611e4dbc7fc403cc087c640378cec585cba4d
ahpalmerUNR <ahpalmer@nevada.unr.edu>
2018-10-15 11:51:13 -0700

    class changes.

--- t3.c
+++ t3.c
@@ -2,12 +2,12 @@
 * @Author: ahpalmerUNR
 * @Date:   2018-10-14 14:05:57
 * @Last Modified by:   ahpalmerUNR
-* @Last Modified time: 2018-10-14 14:31:22
+* @Last Modified time: 2018-10-15 11:17:28
 */
 int main()
 {
 	int one = 1;
-	int two = 2;
+	int two = 1 + 1;
 	
 	char a = 3;
 	
