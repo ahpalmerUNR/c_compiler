5f9bca0e2da607906819f4d4bc47bf5662ba9384
ahpalmerUNR <swordsofold@gmail.com>
2018-10-18 15:20:23 -0700

    Almost ready for turn in.

--- testCases/t2.c
+++ testCases/t2.c
@@ -2,14 +2,19 @@
 * @Author: ahpalmerUNR
 * @Date:   2018-10-11 15:53:15
 * @Last Modified by:   ahpalmerUNR
-* @Last Modified time: 2018-10-18 14:31:04
+* @Last Modified time: 2018-10-18 15:17:01
 */
 !!L 6
 void printf();
 
 int main()
 {
-	
+	struct a {
+			int j;
+			char y;
+			!!D
+	};	
+	enum j = {ALPHA,BETA !!D };
 	int i = 1;
 	float j = 2.3;
 	char testChar = 'a';
