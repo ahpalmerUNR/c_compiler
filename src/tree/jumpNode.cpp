e62092ba5e6ae3d9252808d9e793f4b8b1af43e0
ahpalmerUNR <ahpalmer@nevada.unr.edu>
2018-10-31 12:54:23 -0700

    end lab time commit.

---
+++ src/tree/jumpNode.cpp
@@ -0,0 +1,6 @@
+/*
+* @Author: ahpalmerUNR
+* @Date:   2018-10-31 11:34:10
+* @Last Modified by:   ahpalmerUNR
+* @Last Modified time: 2018-10-31 11:34:10
+*/
---
+++ src/tree/selectionNode.cpp
@@ -0,0 +1,6 @@
+/*
+* @Author: ahpalmerUNR
+* @Date:   2018-10-31 11:49:36
+* @Last Modified by:   ahpalmerUNR
+* @Last Modified time: 2018-10-31 11:49:36
+*/
