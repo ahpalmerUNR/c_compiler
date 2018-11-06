48ee2b6a8e0587157594b7dfb0540b8e17dac226
ahpalmerUNR <ahpalmer@nevada.unr.edu>
2018-11-05 16:03:07 -0800

    started function nodes.

---
+++ src/tree/functionNode.cpp
@@ -0,0 +1,10 @@
+/*
+* @Author: ahpalmerUNR
+* @Date:   2018-11-05 15:44:49
+* @Last Modified by:   ahpalmerUNR
+* @Last Modified time: 2018-11-05 15:45:23
+*/
+#include "functionNode.h"
+
+using namespace std;
+
