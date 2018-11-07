4cb9fc89c2ac7322b2f0982221615668d9139929
Z <zachidk@gmail.com>
2018-11-06 22:24:52 -0800

    done for the night

--- testCases/expressionTest.c
+++ testCases/expressionTest.c
@@ -1,7 +1,12 @@
 // Vector of types for argument expression list, warning on implicit casting
+int q(int l, int f, int t)
+{
+
+}
 int main()
 {
 	int i;
+	int j[5] = {1,2,3,4,5};
 	i = 1 + 5.0;
 	i = 5 + 'c';
 	i = 'c' + 'c';
