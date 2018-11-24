1879b41c4afe591b917c890f20eb9b788a32cf9b
ahpalmerUNR <ahpalmer@nevada.unr.edu>
2018-11-24 13:53:14 -0800

    MIPS sizes added as a .h

---
+++ src/MIPS_Type_Sizes.h
@@ -0,0 +1,15 @@
+#ifndef INT_MIPS
+#define INT_MIPS 4
+#endif
+
+#ifndef CHAR_MIPS
+#define CHAR_MIPS 1
+#endif
+
+#ifndef FLOAT_MIPS
+#define FLOAT_MIPS 4
+#endif
+
+#ifndef DOUBLE_MIPS
+#define DOUBLE_MIPS 8
+#endif
