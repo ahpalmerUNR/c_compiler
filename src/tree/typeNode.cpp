e88fa2db3f74e2716f9d9d63dc21ca987901ebd6
ahpalmerUNR <ahpalmer@nevada.unr.edu>
2018-11-30 08:38:43 -0800

    trying to get ids to carry tickets.

--- src/tree/typeNode.cpp
+++ src/tree/typeNode.cpp
@@ -49,7 +49,8 @@ void TypeNode::traverse_to_file(FILE* fileout)
  */
 void TypeNode::ast_to_3ac(FILE* fileout)
 {
-	int i;
+	int i,tp;
+	char* temp;//[500];
 	// For some reason its always a double unless its like a long int or something and if its long int then int isnt stored?
 	for(i = 0; i < types.size(); i++)
 	{
@@ -75,6 +76,8 @@ void TypeNode::ast_to_3ac(FILE* fileout)
 	byteSize *= children[1]->getArrayOffset();
 	//cout << types[i] << endl;
 	fprintf(fileout,"ALLOC\t(%d)\t%s\n",byteSize,rep_3ac_ticket(types[i],children[1]->returnTicket()).c_str());
+	children[1]->getDataType(temp);
+	astTable.insert(temp,children[1]->forErrors[0].lineStart,children[1]->forErrors[0].colStart,INT_TYPE,&tp);
 }
 
 /**
